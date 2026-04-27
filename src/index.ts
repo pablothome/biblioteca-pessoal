86dbe1fdd2732ab2ba2dc1c1b004c85c18335173
Pablo <pablo55thome@gmail.com>
2026-04-27 13:02:05 -0300

    Implementa cadastro e remoção de livros

--- src/index.ts
+++ src/index.ts
@@ -35,3 +35,57 @@ function exibirBiblioteca(): void {
 }
 
 exibirBiblioteca();
+
+function adicionarLivro(
+  titulo: string,
+  autor: string,
+  ano: number,
+  paginasLivro: number
+): void {
+
+  if (ano <= 0 || paginasLivro <= 0) {
+    console.log('❌ Dados inválidos!');
+    return;
+  }
+
+  titulos.push(titulo);
+  autores.push(autor);
+  anos.push(ano);
+  paginas.push(paginasLivro);
+  lido.push(false);
+  avaliacoes.push(0);
+
+  console.log(`✅ Livro "${titulo}" adicionado com sucesso!`);
+}
+
+function removerLivro(indice: number): void {
+
+  if (indice < 0 || indice >= titulos.length) {
+    console.log('❌ Índice inválido!');
+    return;
+  }
+
+  const tituloRemovido = titulos[indice];
+
+  titulos.splice(indice, 1);
+  autores.splice(indice, 1);
+  anos.splice(indice, 1);
+  paginas.splice(indice, 1);
+  lido.splice(indice, 1);
+  avaliacoes.splice(indice, 1);
+
+  console.log(`🗑️ Livro "${tituloRemovido}" removido!`);
+}
+
+console.log('\n=== TESTE CADASTRO / REMOÇÃO ===');
+
+exibirBiblioteca();
+
+// adicionar
+adicionarLivro('Harry Potter', 'J.K. Rowling', 1997, 300);
+adicionarLivro('Código Limpo 2', 'Robert C. Martin', 2020, 500);
+
+// remover (ex: remove o terceiro livro)
+removerLivro(2);
+
+exibirBiblioteca();
